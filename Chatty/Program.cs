5d187307fa96ec807ef0d878ad39cab39c3d375a
Nakedi <naks702@gmail.com>
2026-03-11 19:38:06 +0200

    Add project files.

---
+++ Chatty/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Media;
+using System;
+
+namespace ChattyApp
+
+{
+    class Program
+       
+
+    {
+
+ public static void PlaySound()
+        {
+            #pragma warning disable CA1416
+            SoundPlayer player = new SoundPlayer(@"C:\Users\Sugar\Documents\Mystudies Second Year\PROG 2A\POE\Welcome Message.wav");
+            player.Play();
+            player.PlaySync();
+          
+            #pragma warning restore CA1416
+        }
+
+
+        public static void Main(string[] args)
+        {
+         PlaySound();
+            
+        }
+
+       
+
+
+    }
+}
