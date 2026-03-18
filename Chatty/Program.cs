6a6b554a5e890215388c43f2ceb677d08ab7902d
Nakedim <alexnaks702@gmail.com>
2026-03-19 01:32:00 +0200

    Working on topical arrarys

--- Chatty/Program.cs
+++ Chatty/Program.cs
@@ -13,7 +13,7 @@ namespace ChattyApp
  public static void PlaySound()
         {
             #pragma warning disable CA1416
-            SoundPlayer player = new SoundPlayer(@"C:\Users\Sugar\Documents\Mystudies Second Year\PROG 2A\POE\Welcome Message.wav");
+            SoundPlayer player = new SoundPlayer(@"C:\Users\Student\source\repos\Chatty_POE_ST10497909\Chatty\Welcome.wav");
             player.Play();
             player.PlaySync();
           
@@ -36,7 +36,8 @@ public static void Greeting(String userAnswer)
                                                                                  
 
             Console.WriteLine(logo);
-            PlaySound();
+            //Paused this method to speeding testing
+            //PlaySound();
             String Welcome = @"  ____               _   _                   _         __   __          _ 
  / ___|_ __ ___  ___| |_(_)_ __   __ _ ___  | |_ ___   \ \ / /__  _   _| |
 | |  _| '__/ _ \/ _ \ __| | '_ \ / _` / __| | __/ _ \   \ V / _ \| | | | |
@@ -45,22 +46,21 @@ public static void Greeting(String userAnswer)
                                  |___/                                    ";
 
 
-            Console.WriteLine("\n");
-            Console.WriteLine("What your is Name");
-            String name = Console.ReadLine();
-            Console.WriteLine(Welcome +  name +" Welcome to the App");
-            Console.WriteLine("\n");
-            Console.WriteLine("How are You " + name);
+            /*   Console.WriteLine("\n");
+               Console.WriteLine("What your is Name");
+               String name = Console.ReadLine();
+               Console.WriteLine(Welcome +  name +" Welcome to the App");
+               Console.WriteLine("\n");
+               Console.WriteLine("How are You " + name);
 
 
 
-           Console.WriteLine("What is your purpose?");
-            String question_1 = Console.ReadLine();
-            Console.WriteLine();
-           Console.WriteLine("What can i ask you About?");
-            String question_2 = Console.ReadLine();
+              Console.WriteLine("What is your purpose?");
+               String question_1 = Console.ReadLine();
+               Console.WriteLine();*/
+
             
-
+           
             //Storing answering
             String answer_1 = "Password safety include making sure the password is made up of characters, numeric and letters";
             String answer_2 = "Do not click onto unknowing links and email";
@@ -69,43 +69,68 @@ public static void Greeting(String userAnswer)
             //Storing list of topics 
             String[] topics = { "Password Safety", "Phishing","Safe Browsing" };
 
-            for (int i  =0; i <topics.Length; i++) 
+            Console.WriteLine("What can i ask you About?");
+            String question_2 = Console.ReadLine();
+
+           
+
+            //check if input is empty
+            while (string.IsNullOrEmpty(question_2)) 
             {
-                if (topics[i].Contains(question_2))
-                {
-                 
-                 Console.WriteLine("The answer " + answer_1);
-                    break;
+                Console.WriteLine("re-enter your response!");
+                question_2 = Console.ReadLine();
 
-                }
-                if (topics[i] =="Phishing")
-                {
-                   
-                 Console.WriteLine("The answer " + answer_2);
-                    break;
-                }
-                if (topics[i]=="Browsing")
-                    break;
-                {
-                 
-                 Console.WriteLine("The answer " + answer_3);
-                }
+            }
+            Console.WriteLine("Choose the following topics:\n1:Password Safety\n2:Phishing\n3:Safe Browsing");
+            string response = Console.ReadLine();
+            int res = Int32.Parse(response);
 
+            switch (res)
+            {
+                    case 1:
+                    Console.WriteLine("Chatty Bot: " +answer_1);
+                    break;
+                    case 2:
+                    Console.WriteLine("Chatty Bot: " + answer_2);
+                    break;
+                    case 3:
+                    Console.WriteLine("Chatty Bot: " + answer_3);
+                    break;
+                default:
+                    Console.WriteLine("Invalid response: Enter number between 1 - 3");
+                    break;
+                    
 
+            }
+            Console.WriteLine("is there anything i can assist you with");
+            switch (res)
+            {
+                case 1:
+                Console.WriteLine("What is your purpose");
+                    break;
+            }
+            
+            String question_3 = Console.ReadLine();
+            while (string.IsNullOrEmpty(question_3))
+            {
+                Console.WriteLine("re-enter your response!");
+                question_3 = Console.ReadLine();
 
             }
 
 
 
-            
 
-            
-            
 
-            
-            
 
-           
+
+
+
+
+
+
+
+
 
 
         }
