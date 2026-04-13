63c2870d22d9f7ce01e4f96414d6306c5a7941f1
Nakedi <naks702@gmail.com>
2026-04-13 16:53:45 +0200

    Working app console interface

--- Chatty/BotData.cs
+++ Chatty/BotData.cs
@@ -41,35 +41,38 @@ namespace Chatty
                    
                 }
 
+                //We use StringComparison as it is case-insenstive
                 if (UserResponse.Contains("password", StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine("Password must be a mix of alphaNumeric character, new share your password ");
                     UserResponse = Console.ReadLine();
+                    //Thread.Sleep(1000);
+                    // Console.WriteLine("Typing");
+                }
+                string LetsContinue;
                 do
                 {
-                    Console.WriteLine("Do you want to want to search another topic: Press Y to continue or any key to Quit");
+                    Console.WriteLine("Enter the topic you want to search:");
+                    string topic = Console.ReadLine();
+                    Console.WriteLine("Do you want to want to search another topic: Press y to continue or any key to Quit");
+                    LetsContinue = Console.ReadLine().Trim().ToLower();
 
-                } while (Console.ReadLine().Trim().ToLower() == "y");
-
-          
+                } while (LetsContinue == "y");
+                if (LetsContinue != null )
+                    LetsContinue = Console.ReadLine().Trim().ToLower();
+                {
+                    Console.WriteLine();
                 }
-                
                 if (UserResponse.Contains("phishing", StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine("Do not Open unknown email links");
                     UserResponse = Console.ReadLine();
                     continue;
-
                 }
                 break;
 
-
-                
             }
 
-
-
-            
 
             Console.WriteLine("Whats your purpose");
             Console.WriteLine("What can i ask you about");
@@ -78,6 +81,8 @@ namespace Chatty
 
 
         }
+
+
 
 
     }
--- Chatty/GUI.cs
+++ Chatty/GUI.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Media;
 using System.Text;
 
 namespace Chatty
 {
     internal class GUI
     {
+        private static String logo = @"  ____      _               ____                       _ _                _                
+ / ___|   _| |__   ___ _ __/ ___|  ___  ___ _   _ _ __(_) |_ _   _       / \   _ __  _ __  
+| |  | | | | '_ \ / _ \ '__\___ \ / _ \/ __| | | | '__| | __| | | |     / _ \ | '_ \| '_ \ 
+| |__| |_| | |_) |  __/ |   ___) |  __/ (__| |_| | |  | | |_| |_| |    / ___ \| |_) | |_) |
+ \____\__, |_.__/ \___|_|  |____/ \___|\___|\__,_|_|  |_|\__|\__, |   /_/   \_\ .__/| .__/ 
+      |___/                                                  |___/            |_|   |_|    ";
+
+        public string Name { get; set; }
+
+        public  void PlaySound(User user)
+        {
+            Console.WriteLine(logo);
+            SoundPlayer player = new SoundPlayer(@"C:\Users\Sugar\source\repos\Chatty\Chatty\Welcome Message.wav");
+            //player.PlaySync();
+            Console.WriteLine("\n");
+            Console.WriteLine("Typing...");
+            Thread.Sleep(2000);
+            Console.WriteLine("What your is Name");
+            user.Name = Console.ReadLine();
+
+
+            Console.WriteLine("Typing...");
+            Thread.Sleep(2000);
+          
+
+
+
+
+
+
+        }
     }
 }
