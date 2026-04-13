d25f18703f4135e554b9912dbdee66735c45233a
Nakedi <naks702@gmail.com>
2026-04-13 23:59:53 +0200

    final commit

--- Chatty/User.cs
+++ Chatty/User.cs
@@ -9,18 +9,5 @@ namespace Chatty
         public string Name { get; set; }
     }
 
-    //We use StringComparison as it is case -insenstive
-    //if (UserReply.Contains("Fine", StringComparison.OrdinalIgnoreCase))
-    //    {
-    //        Console.WriteLine("Im glad to hear that, im also good");
-    //        Thread.Sleep(1500);
-    //        Console.WriteLine("Typing");
 
-    //    }
-    //if (UserReply.Contains("You", StringComparison.OrdinalIgnoreCase))
-    //{
-    //    Console.WriteLine("I'm also good");
-    //    Thread.Sleep(1500);
-    //    Console.WriteLine("Typing");
-    //}
 }
